de17d8e61f1adc898e1d06e1168a7779a3c7c4c3
Nelson <nelsonmggomes@gmail.com>
2020-09-01 22:26:32 -0400

    UPDATE: Logout link

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,25 @@
 import React, { useContext } from 'react';
 import './Navbar.css'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { UserContext } from '../App'
 
 const NavBar = () => {
     const {state, dispatch} = useContext(UserContext);
+    const history = useHistory();
     const renderList = () => {
         if(state){
             return [
                 <li><Link to="/"><i className="small material-icons">home</i></Link></li>,
                 <li><Link to="#"><i className="small material-icons">favorite_border</i></Link></li>,
                 <li><Link to="/create"><i className="small material-icons">add_circle</i></Link></li>,
-                <li><Link to="/profile">My profile</Link></li>
+                <li><Link to="/profile">My profile</Link></li>,
+                <li><Link 
+                onClick={() => {
+                        localStorage.clear()
+                        dispatch({ type: "CLEAR" })
+                        history.push('/signin')
+                    }
+                }><i className="small material-icons">exit_to_app</i></Link></li>
             ]
         } else {
             return [
